d7d8020dad2532cbd613a38b280ac6f4b7fe4fc7
alisariaslann <ali.sariaslan@desenerp.com>
2023-10-04 22:00:34 +0300

    lolization

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-// MODULES
+//MODULES
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var routes = require('./router/router');
+var i18n = require('i18n');
 
-// USE
+//LOCALIZATION
+i18n.configure({
+  locales: ['en', 'tr'],
+  defaultLocale: 'en',
+  directory: './public/locales',
+  cookie: 'locale',
+  objectNotation: true,
+});
+
+//ENGINE
+app.set('view engine', 'ejs');
+
+//USE
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
+app.use(i18n.init);
+
+app.use((req, res, next) => {
+  const userLang = req.headers['accept-language'];
+  req.setLocale(userLang);
+  next();
+});
+
 app.use(routes);
 
-//ENGINE
-app.set('view engine', 'ejs');
-
-// Port ve host değişkenlerini tanımla
+//DEFINITON FOR HOST AND PORT
 var port = 10101;
 var host = 'localhost';
 
-// Web sunucusunu başlat
+//START WEBSERVER
 function start_http() {
   var server = app.listen(port, host, function () {
     console.log('Web server started at -> http://%s:%s', host, port);
